537a167419142a8f992cec81d08c28f8189f693d
Tiago Castro <tiagocastro1000@gmail.com>
2025-03-18 23:03:15 +0000

    feat: communications between backend and frontend done and working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+//
+// @Component({
+//   selector: 'app-root',
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.scss'
+// })
+// export class AppComponent {
+//   title = 'ASID-Temp';
+// }
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BookListComponent } from './components/book-list/book-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  standalone: true,
+  imports: [BookListComponent],
+  template: `<app-book-list></app-book-list>`,
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ASID-Temp';
-}
+export class AppComponent { }
