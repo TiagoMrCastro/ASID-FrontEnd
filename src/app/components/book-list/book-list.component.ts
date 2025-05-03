c51441c88523c44478ce70da2c5db5b9d1b466a8
Carlos Gonçalo Castro Campos <pg57842@alunos.uminho.pt>
2025-05-03 13:11:58 +0100

    feat: updates

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BookService } from '../../services/book.service';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule,MatToolbarModule],
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.scss']
 })
 export class BookListComponent implements OnInit {
   books: any[] = [];
-
-  constructor(private bookService: BookService) {}
-
+  constructor(private bookService: BookService, private http: HttpClient) {}
+  imageList: string[] = [];
   ngOnInit(): void {
     this.loadBooks();
     console.log(this.books)
@@ -24,6 +27,13 @@ export class BookListComponent implements OnInit {
       this.books = data;
     });
   }
+
+  getImageFilename(title: string): string {
+    return title.toLowerCase().replace(/\s+/g, '-') + '.jpg';
+  }
+  
+  
+  
 
   deleteBook(id: number) {
     this.bookService.deleteBook(id).subscribe(() => {
